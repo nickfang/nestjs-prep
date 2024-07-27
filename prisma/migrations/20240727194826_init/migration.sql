a9fbcbe364126c303f253cde0f9c5a703ae04f2c
Nick Fang <fang.nicholas@gmail.com>
2024-07-27 14:50:12 -0500

    Added an initial schema for a recipe book.  Added a local.yml file to run postgres in docker.  created an initial migration using npx prisma migrate dev --name init.

---
+++ prisma/migrations/20240727194826_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "instructions" TEXT NOT NULL,
+    "servings" INTEGER NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ingredient" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "unit" TEXT NOT NULL,
+    "recipeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Ingredient_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Ingredient" ADD CONSTRAINT "Ingredient_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
